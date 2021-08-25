a3a213d6603b900aa500ca6208760be07b981e6d
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2021-08-25 09:39:46 -0500

    Edited EnemyMove
    Added aggro range, still needs some tweaks

--- IronChefUnity/Iron Chef/Assets/Scripts/EnemyMove.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/EnemyMove.cs
@@ -6,16 +6,32 @@ using UnityEngine.AI;
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private Transform moveTowards;
+    [SerializeField] private int aggroDistance;
+    [SerializeField] private int moveRange;
+    [SerializeField] private float speed = 5f;
+    private Vector3 startPosition;
     private NavMeshAgent agent;
+    private bool isReturning;
 
     private void OnEnable()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        startPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = moveTowards.position;
+        if (Vector3.Distance(moveTowards.position, gameObject.transform.position) < aggroDistance &&
+            Vector3.Distance(startPosition, gameObject.transform.position) < moveRange)
+        {
+            agent.destination = moveTowards.position;
+            agent.speed = speed;
+        }
+        else
+        {
+            agent.destination = startPosition;
+            agent.speed = speed / 2;
+        }
     }
 }
