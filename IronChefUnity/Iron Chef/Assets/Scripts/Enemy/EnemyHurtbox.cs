a907dfe4ec7b39de4e10028166f311380f26930c
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2021-08-30 08:28:59 -0500

    Enemy hurtbox
    The enemy now does damage on contact with the player, and the player has invincibility frames after getting hit.

--- IronChefUnity/Iron Chef/Assets/Scripts/Enemy/EnemyHurtbox.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/Enemy/EnemyHurtbox.cs
@@ -2,31 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyProjectileControl : MonoBehaviour
+public class EnemyHurtbox : MonoBehaviour
 {
     [Tooltip("Float for how much damage the projectile does on contact with the player.")]
     [SerializeField] private float damage;
-    [Tooltip("Float for how fast the projectile moves.")]
-    [SerializeField] private float speed;
-    private Vector3 currentPosition;
     private PlayerHitpoints playerHitpoints;
-
-    private void Update()
-    {
-        currentPosition = gameObject.transform.position;
-        gameObject.transform.position += transform.forward * speed * Time.deltaTime;
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         playerHitpoints = other.gameObject.GetComponentInParent<PlayerHitpoints>();
         if (playerHitpoints != null)
         {
-            Debug.Log("Hurt!");
-
             //TODO: Check for status effects for on hit things
             playerHitpoints.TakeDamage(damage);
         }
-        Destroy(gameObject);
     }
 }
--- IronChefUnity/Iron Chef/Assets/Scripts/PlayerHitpoints.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/PlayerHitpoints.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PlayerHitpoints : MonoBehaviour
 {
+    //This maybe should be moved to playerStats
+    [Tooltip("Float for how much time the player's invicibility after being hit lasts.")]
+    [SerializeField] private float IFramesAmount;
     private PlayerStats playerStats;
+    private Animator anim;
+    private bool isIFrames = false;
 
     private void Awake()
     {
         playerStats = gameObject.GetComponent<PlayerStats>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     public void TakeDamage(float amount)
     {
         //TODO: Check powers/status effects for taking damage
-
-        playerStats.CurrentHP -= amount;
-
-        if (playerStats.CurrentHP <= 0)
+        if (isIFrames == false)
         {
-            Die();
+            Debug.Log("Hurt!");
+
+            //Play animation of getting hit here. I'm not sure yet if the animation
+            //will include knockback or not so I won't include it yet.
+            isIFrames = true;
+            Invoke("isIFramesEnd", IFramesAmount);
+
+            playerStats.CurrentHP -= amount;
+
+            if (playerStats.CurrentHP <= 0)
+            {
+                Die();
+            }
+            Debug.Log(isIFrames);
         }
     }
     public void Die()
@@ -28,5 +44,10 @@ public class PlayerHitpoints : MonoBehaviour
 
         //TODO: Play death animation before deletion
 
+    }
+
+    private void isIFramesEnd()
+    {
+        isIFrames = false;
     }
 }
