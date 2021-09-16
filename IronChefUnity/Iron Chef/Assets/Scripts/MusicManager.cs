96ea2a45e920cf99d6b2bf6f201895bb178857b2
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-09-16 11:02:37 -0500

    Scene + Build

---
+++ IronChefUnity/Iron Chef/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource normalMusicSource;
+    public AudioSource combatMusicSource;
+    public List<AudioClip> normalClips;
+    public List<AudioClip> combatClips;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
