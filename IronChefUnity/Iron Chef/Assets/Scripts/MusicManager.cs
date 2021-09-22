702f53d3cee9ab0982754c83d5fc7943ed8b4f9e
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-09-22 13:06:23 -0500

    In Combat/Out of Combat music swapping

--- IronChefUnity/Iron Chef/Assets/Scripts/MusicManager.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/MusicManager.cs
@@ -9,17 +9,81 @@ public class MusicManager : MonoBehaviour
     public List<AudioClip> normalClips;
     public List<AudioClip> combatClips;
 
+    GameObject player;
 
+    int currentNormalClip;
+    int currentCombatClip;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<CharacterMover>().gameObject;
+
+        currentNormalClip = 0;
+        currentCombatClip = 0;
+        normalMusicSource.clip = normalClips[0];
+        combatMusicSource.clip = combatClips[0];
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        updateMusicVolume();
+        updateMusicClips();
+    }
+
+    void updateMusicVolume()
+    {
+        if (player != null)
+        {
+            //TODO: MAKE BETTER CHECK
+            bool inCombat = false;
+
+
+            foreach (var e in FindObjectsOfType<GenericEnemyBehavior>())
+            {
+                if (Vector3.Distance(player.transform.position, e.gameObject.transform.position) <= 20)
+                {
+                    inCombat = true;
+                    break;
+                }
+            }
+
+            if (inCombat)
+            {
+                combatMusicSource.volume = Mathf.Clamp(combatMusicSource.volume + Time.deltaTime, 0, 1);
+                normalMusicSource.volume = Mathf.Clamp(normalMusicSource.volume - Time.deltaTime, 0, 1);
+            }
+            else
+            {
+                combatMusicSource.volume = Mathf.Clamp(combatMusicSource.volume - Time.deltaTime, 0, 1);
+                normalMusicSource.volume = Mathf.Clamp(normalMusicSource.volume + (Time.deltaTime / 3f), 0, 1);
+            }
+        }
         
+    }
+    void updateMusicClips()
+    {
+        if(normalMusicSource.isPlaying == false)
+        {
+            currentNormalClip++;
+            if(currentNormalClip >= normalClips.Count)
+            {
+                currentNormalClip = 0;
+            }
+            normalMusicSource.clip = normalClips[currentNormalClip];
+            normalMusicSource.Play();
+        }
+        if (combatMusicSource.isPlaying == false)
+        {
+            currentCombatClip++;
+            if (currentCombatClip >= combatClips.Count)
+            {
+                currentCombatClip = 0;
+            }
+            combatMusicSource.clip = combatClips[currentCombatClip];
+            combatMusicSource.Play();
+        }
     }
 }
