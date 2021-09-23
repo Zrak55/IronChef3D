89ff785bad70a7708ef3f90fb41235c9ef13755f
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-09-23 11:37:17 -0500

    ballin bars

--- IronChefUnity/Iron Chef/Assets/Scripts/Player/PlayerHUDManager.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/Player/PlayerHUDManager.cs
@@ -12,20 +12,27 @@ public class PlayerHUDManager : MonoBehaviour
 
     public Slider hpBar;
     public Slider staminaBar;
+    float targetHPValue;
 
-    [Space]
+    [Header("Food Bars")]
     public Slider food1bar;
     public Slider food2bar;
     public Slider food3bar;
     public Slider food4bar;
     public Slider food5bar;
     public Slider badfoodbar;
-
+    [Space]
     public Image food1requiredbar;
     public Image food2requiredbar;
     public Image food3requiredbar;
     public Image food4requiredbar;
     public Image food5requiredbar;
+    [Space]
+    public Text food1ingredient;
+    public Text food2ingredient;
+    public Text food3ingredient;
+    public Text food4ingredient;
+    public Text food5ingredient;
 
     private void Awake()
     {
@@ -50,6 +57,7 @@ public class PlayerHUDManager : MonoBehaviour
         food5bar.maxValue = foodInfo.ingredientFiveMaximum;
         food5bar.value = foodInfo.ingredientFiveRequired;
         food5requiredbar.transform.position = food5bar.handleRect.transform.position;
+        food5ingredient.text = foodInfo.ingredientFiveType.ToString();
         if (food5bar.maxValue == 0)
         {
             food5bar.gameObject.SetActive(false);
@@ -60,7 +68,8 @@ public class PlayerHUDManager : MonoBehaviour
 
         food4bar.maxValue = foodInfo.ingredientFourMaximum;
         food4bar.value = foodInfo.ingredientFourRequired;
-        food4requiredbar.transform.position = food4bar.handleRect.transform.position; 
+        food4requiredbar.transform.position = food4bar.handleRect.transform.position;
+        food4ingredient.text = foodInfo.ingredientFourType.ToString();
         if (food4bar.maxValue == 0)
         {
             food4bar.gameObject.SetActive(false);
@@ -70,6 +79,7 @@ public class PlayerHUDManager : MonoBehaviour
         food3bar.maxValue = foodInfo.ingredientThreeMaximum;
         food3bar.value = foodInfo.ingredientThreeRequired;
         food3requiredbar.transform.position = food3bar.handleRect.transform.position;
+        food3ingredient.text = foodInfo.ingredientThreeType.ToString();
         if (food3bar.maxValue == 0)
         {
             food3bar.gameObject.SetActive(false);
@@ -79,6 +89,7 @@ public class PlayerHUDManager : MonoBehaviour
         food2bar.maxValue = foodInfo.ingredientTwoMaximum;
         food2bar.value = foodInfo.ingredientTwoRequired;
         food2requiredbar.transform.position = food2bar.handleRect.transform.position;
+        food2ingredient.text = foodInfo.ingredientTwoType.ToString();
         if (food2bar.maxValue == 0)
         {
             food2bar.gameObject.SetActive(false);
@@ -88,6 +99,7 @@ public class PlayerHUDManager : MonoBehaviour
         food1bar.maxValue = foodInfo.ingredientOneMaximum;
         food1bar.value = foodInfo.ingredientOneRequired;
         food1requiredbar.transform.position = food1bar.handleRect.transform.position;
+        food1ingredient.text = foodInfo.ingredientOneType.ToString();
         if (food1bar.maxValue == 0)
         {
             food1bar.gameObject.SetActive(false);
@@ -104,20 +116,21 @@ public class PlayerHUDManager : MonoBehaviour
         UpdateFoodBars();
     }
 
+
     private void UpdateHPBar()
     {
-        hpBar.value = stats.CurrentHP / stats.MaximumHP;
+        moveABar(hpBar, stats.CurrentHP / stats.MaximumHP);
     }
     private void UpdateStaminaBar()
     {
-        staminaBar.value = stats.CurrentStamina / stats.MaximumStamina;
+        moveABar(staminaBar, stats.CurrentStamina / stats.MaximumStamina);
     }
 
     private void UpdateFoodBars()
     {
         Color targetcolor;
 
-        food1bar.value = foodInfo.ingredientOneCurrent;
+        moveABar(food1bar, foodInfo.ingredientOneCurrent);
         if(foodInfo.ingredientOneCurrent > foodInfo.ingredientOneRequired)
         {
             targetcolor = Color.red;
@@ -138,7 +151,7 @@ public class PlayerHUDManager : MonoBehaviour
         }
 
 
-        food2bar.value = foodInfo.ingredientTwoCurrent;
+        moveABar(food2bar, foodInfo.ingredientTwoCurrent);
         if (foodInfo.ingredientTwoCurrent > foodInfo.ingredientTwoRequired)
         {
             targetcolor = Color.red;
@@ -158,7 +171,7 @@ public class PlayerHUDManager : MonoBehaviour
                 i.color = targetcolor;
         }
 
-        food3bar.value = foodInfo.ingredientThreeCurrent;
+        moveABar(food3bar, foodInfo.ingredientThreeCurrent);
         if (foodInfo.ingredientThreeCurrent > foodInfo.ingredientThreeRequired)
         {
             targetcolor = Color.red;
@@ -178,7 +191,7 @@ public class PlayerHUDManager : MonoBehaviour
                 i.color = targetcolor;
         }
 
-        food4bar.value = foodInfo.ingredientFourCurrent;
+        moveABar(food4bar, foodInfo.ingredientFourCurrent);
         if (foodInfo.ingredientFourCurrent > foodInfo.ingredientFourRequired)
         {
             targetcolor = Color.red;
@@ -198,7 +211,7 @@ public class PlayerHUDManager : MonoBehaviour
                 i.color = targetcolor;
         }
 
-        food5bar.value = foodInfo.ingredientFiveCurrent;
+        moveABar(food5bar, foodInfo.ingredientFiveCurrent);
         if (foodInfo.ingredientFiveCurrent > foodInfo.ingredientFiveRequired)
         {
             targetcolor = Color.red;
@@ -218,6 +231,46 @@ public class PlayerHUDManager : MonoBehaviour
                 i.color = targetcolor;
         }
 
+    }
+
+    public void slowMoveSlider(Slider s, float target)
+    {
+        StartCoroutine(moveSlider(s, s.value, target));
+    }
+    
+    IEnumerator moveSlider(Slider s, float init, float target)
+    {
+        float distance = target - init;
+        float tickRatePerSecond = distance / 0.5f;
+        float t = 0;
+        while (t < 0.5f)
+        {
+            if(target > init)
+                s.value = Mathf.Clamp(s.value + (tickRatePerSecond * Time.deltaTime), s.minValue, target);
+            else
+                s.value = Mathf.Clamp(s.value + (tickRatePerSecond * Time.deltaTime), target, s.maxValue);
+
+            t += Time.deltaTime;
+            yield return null;
+        }
+        s.value = target;
+    }
+
+
+    void moveABar(Slider s, float target)
+    {
+        if (s.value != target)
+        {
+            float tickRate = Mathf.Clamp(Mathf.Abs(s.value - target) * Time.deltaTime * 0.5f * (s.maxValue - s.minValue), Time.deltaTime * 2, Mathf.Infinity);
+            if (Mathf.Abs(target - s.value) < tickRate)
+                s.value = target;
+            else
+            {
+                if (target < s.value)
+                    tickRate *= -1;
+                s.value = s.value + tickRate;
+            }
+        }
     }
 
 }
