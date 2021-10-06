3522a2d5284a976f8bcb066bf88d46a6068fa592
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2021-10-06 09:15:56 -0500

    Snakon
    Added new version of Snakon into unity. This includes animations and base script. Isn't working properly now, still needs work.

---
+++ IronChefUnity/Iron Chef/Assets/Scripts/Enemy/Behavior Tree/SnakonBehavior.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SnakonBehavior : MonoBehaviour
+{
+    BehaviorTree snakonBehaviorTree;
+    [Tooltip("Float for the maximum disatnce the enemy will begin to follow the player from.")]
+    [SerializeField] private float aggroRange = 10;
+    [Tooltip("Float for the maximum disatnce the enemy will move from the spawn.")]
+    [SerializeField] private float spawnRange = 30;
+    [Tooltip("Float for the maximum distance the enemy will begin to attack from.")]
+    [SerializeField] private float attackRange = 5;
+    [Tooltip("Float for the time it takes the enemy to attack. (Will be replaced later with animation).")]
+    [SerializeField] private float attackTime = 4;
+    [Tooltip("Float for the time between the enemy's attack")]
+    [SerializeField] private float attackCD = 2;
+    private Transform player;
+    private Animator animator;
+    private NavMeshAgent agent;
+    private EnemyHitpoints enemyHitpoints;
+    private EnemyBasicAttackbox enemyBasicAttackbox;
+    //Nodes for the behavior tree. Will be adding more later.
+    private Node CheckPlayer, CheckHurt, CheckAttack, ResetMove, MoveTowardsPlayer, PlayerSpawnRange, PlayerAggroRange, EnemyHurt, PlayerAttackRange, Attack;
+    //The spawn location of the enemy is automatically set based on scene placement.
+    private Vector3 startPosition;
+    //Ensure the enemy doesn't start a new attack in the middle of an old one.
+    private bool isAttacking = false, isAttackCD = false;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        enemyBasicAttackbox = GetComponent<EnemyBasicAttackbox>();
+        if (enemyBasicAttackbox == null)
+            enemyBasicAttackbox = GetComponentInChildren<EnemyBasicAttackbox>();
+        enemyHitpoints = GetComponent<EnemyHitpoints>();
+        animator = GetComponent<Animator>();
+        player = GameObject.Find("Player").transform;
+
+        //Setup leaf nodes
+        EnemyHurt = new Leaf("Enemy Hurt?", checkEnemyHurt);
+        PlayerSpawnRange = new Leaf("Player in Spawn Range?", checkSpawnRange);
+        PlayerAggroRange = new Leaf("Player in Aggro Range?", checkAggroRange);
+        PlayerAttackRange = new Leaf("Player in Attack Range?", checkPlayerAttackRange);
+        MoveTowardsPlayer = new Leaf("Move towards player", moveTowards);
+        ResetMove = new Leaf("Reset Move", movePause);
+        Attack = new Leaf("Attack", attack);
+
+        //Setup sequence nodes and root
+        CheckPlayer = new Sequence("Player Location Sequence", PlayerSpawnRange, PlayerAggroRange, MoveTowardsPlayer);
+        CheckHurt = new Sequence("Check Hurt Sequence", EnemyHurt, MoveTowardsPlayer);
+        CheckAttack = new Sequence("Attack Sequence", PlayerAttackRange, Attack);
+        snakonBehaviorTree = new BehaviorTree(ResetMove, CheckPlayer, CheckHurt, CheckAttack);
+    }
+
+    //TODO: Fix multiple things same frame.
+    private void Update()
+    {
+        snakonBehaviorTree.behavior();
+    }
+
+    //This is intended to be running in the update function through the behavior tree.
+    public Node.STATUS moveTowards()
+    {
+        animator.SetBool("isMoving", true);
+        agent.destination = player.transform.position;
+        MoveTowardsPlayer.status = Node.STATUS.SUCCESS;
+        return MoveTowardsPlayer.status;
+    }
+
+    public Node.STATUS movePause()
+    {
+        agent.destination = startPosition;
+        if (agent.velocity == Vector3.zero)
+            animator.SetBool("isMoving", false);
+        ResetMove.status = Node.STATUS.SUCCESS;
+        return ResetMove.status;
+    }
+
+    public Node.STATUS attack()
+    {
+        //When the attack animation has finished this will play.
+        if (Attack.status == Node.STATUS.RUNNING && !isAttacking)
+        {
+            isAttackCD = true;
+            Invoke("attackCDEnd", attackCD);
+            enemyBasicAttackbox.HitOn();
+
+            Attack.status = Node.STATUS.SUCCESS;
+            return Attack.status;
+        }
+        //If we aren't already attack and the cd is done, then attack.
+        else if (!isAttacking && !isAttackCD)
+        {
+            isAttacking = true;
+            animator.SetTrigger("Attack");
+            Invoke("attackEnd", attackTime);
+            enemyBasicAttackbox.HitOff();
+
+            agent.destination = transform.position;
+        }
+        Attack.status = Node.STATUS.RUNNING;
+        return Attack.status;
+    }
+
+    private void attackEnd()
+    {
+        isAttacking = false;
+    }
+
+    private void attackCDEnd()
+    {
+        isAttackCD = false;
+    }
+
+    public Node.STATUS checkEnemyHurt()
+    {
+        if (enemyHitpoints.damaged)
+        {
+            EnemyHurt.status = Node.STATUS.SUCCESS;
+            return EnemyHurt.status;
+        }
+        EnemyHurt.status = Node.STATUS.FAILURE;
+        return EnemyHurt.status;
+    }
+
+    public Node.STATUS checkAggroRange()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < aggroRange)
+        {
+            PlayerAggroRange.status = Node.STATUS.SUCCESS;
+            return PlayerAggroRange.status;
+        }
+        PlayerAggroRange.status = Node.STATUS.FAILURE;
+        return PlayerAggroRange.status;
+    }
+
+    public Node.STATUS checkSpawnRange()
+    {
+        if (Vector3.Distance(player.transform.position, startPosition) < spawnRange)
+        {
+            PlayerSpawnRange.status = Node.STATUS.SUCCESS;
+            return PlayerSpawnRange.status;
+        }
+        PlayerSpawnRange.status = Node.STATUS.FAILURE;
+        return PlayerSpawnRange.status;
+    }
+
+    public Node.STATUS checkPlayerAttackRange()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < attackRange)
+        {
+            PlayerAttackRange.status = Node.STATUS.SUCCESS;
+            return PlayerAttackRange.status;
+            return PlayerAttackRange.status;
+        }
+        PlayerAttackRange.status = Node.STATUS.FAILURE;
+        return PlayerAttackRange.status;
+    }
+
+    
+
+    
+}
