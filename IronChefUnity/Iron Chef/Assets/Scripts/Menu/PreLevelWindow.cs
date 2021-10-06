8453d5ce0456221887a40623ee7c3e2975356783
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-10-06 09:49:22 -0500

    A lot of stuff

--- IronChefUnity/Iron Chef/Assets/Scripts/Menu/PreLevelWindow.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/Menu/PreLevelWindow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PreLevelWindow : MonoBehaviour
@@ -8,13 +9,19 @@ public class PreLevelWindow : MonoBehaviour
     public string levelName;
     public Text descText;
 
+    private void Awake()
+    {
+        EventSystem.current.SetSelectedGameObject(GetComponentInChildren<Button>().gameObject);
+    }
     public void Hide()
     {
+        EventSystem.current.SetSelectedGameObject(GetComponentInParent<GameObject>().GetComponentInChildren<Button>().gameObject);
         gameObject.SetActive(false);
     }
     public void Play()
     {
         FindObjectOfType<MenuController>().PlayGame(levelName);
+        
     }
 
 
