dc09c801c711d37cc79bca4bc40a1f14af32c199
zach1sboyled <59011633+zach1sboyled@users.noreply.github.com>
2021-10-18 08:42:35 -0500

    fence get

---
+++ IronChefUnity/Iron Chef/Assets/Scripts/TreeRando.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treerandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int RandomShader = Random.Range(0, 10);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
