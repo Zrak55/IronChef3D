49dcbb14ab848477e6d46964bc2b3cbeecf16620
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-10-26 10:30:47 -0500

    More post processing

--- IronChefUnity/Iron Chef/Assets/Scripts/LeafColorChanger.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/LeafColorChanger.cs
@@ -11,17 +11,6 @@ public class LeafColorChanger : MonoBehaviour
     [Range(0, 1)]
     public float percentChangeChance;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 
     public void ChangeColor()
