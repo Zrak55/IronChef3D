e834886d23e703776ca3850b9d6cfab03f1f102b
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-10-27 08:58:16 -0500

    Skyboxes, Textures, and more

--- IronChefUnity/Iron Chef/Assets/Scripts/Enemy/BenedictRoll.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/Enemy/BenedictRoll.cs
@@ -129,7 +129,7 @@ public class BenedictRoll : MonoBehaviour
         animator.speed = 0;
         startingRoll = true;
 
-        FindObjectOfType<SoundEffectSpawner>().MakeSoundEffect(transform.position, SoundEffectSpawner.SoundEffect.EggRollStart);
+        FindObjectOfType<SoundEffectSpawner>().MakeSoundEffect(transform.position + ((FindObjectOfType<CharacterMover>().transform.position - transform.position)/2), SoundEffectSpawner.SoundEffect.EggRollStart);
 
         Invoke("EndRolling", time);
     }
