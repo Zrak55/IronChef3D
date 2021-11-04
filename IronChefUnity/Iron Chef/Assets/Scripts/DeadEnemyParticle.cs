df234861af37c385425ee2e4e7ef74a89eeec6fd
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2021-11-04 12:06:43 -0500

    Bug fixes + enemy exploding

--- IronChefUnity/Iron Chef/Assets/Scripts/DeadEnemyParticle.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/DeadEnemyParticle.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class DeadEnemyParticle : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    Vector3 spin;
+    public float spinRateDegrees = 180f;
+    
     void Start()
     {
-        
+        spin = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(spin * spinRateDegrees * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
