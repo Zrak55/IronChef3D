018f8266a13b48c500e8db81962e5646bb992514
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2021-11-30 17:31:10 -0600

    Meatling done
    Meatlings should be working properly. They spawn in fine and push around the player like with the grits. Also fixed the fade out text because it was annoying, players honestly wont run into that problem.

--- IronChefUnity/Iron Chef/Assets/Scripts/Enemy/Bosses/MeatosaurusRoar.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/Enemy/Bosses/MeatosaurusRoar.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class MeatosaurusRoar : MonoBehaviour
 {
     public List<Transform> MeatlingSpawns;
+    private List<Vector3> MeatlingSpawnV;
     public GameObject Meatling;
+
+    private void Start()
+    {
+        MeatlingSpawnV = new List<Vector3>();
+        foreach (Transform transform in MeatlingSpawns)
+            MeatlingSpawnV.Add(transform.position);
+    }
 
     public void SpawnMeatlings()
     {
-
+        foreach (Vector3 vector3 in MeatlingSpawnV)
+        {
+            Debug.Log(vector3);
+            Instantiate(Meatling, vector3, new Quaternion());
+        }
     }
 }
