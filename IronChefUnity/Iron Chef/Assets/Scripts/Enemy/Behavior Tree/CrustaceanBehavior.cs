d3dd00a5cd18f26a00ddfa2763af0509bd819047
Spencersxtn <33520072+Spencersxtn@users.noreply.github.com>
2022-02-16 08:44:05 -0600

    Crustacean Hide
    Also increased tomatroll jump range.  The hide doesn't have an animation yet, he just freezes right now.

--- IronChefUnity/Iron Chef/Assets/Scripts/Enemy/Behavior Tree/CrustaceanBehavior.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/Enemy/Behavior Tree/CrustaceanBehavior.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class CrustaceanBehavior : EnemyBehaviorTree
 {
     BehaviorTree crustaceanBehaviorTree;
-    private Node CheckPlayer, CheckHurt, CheckAttack;
+    private Node CheckPlayer, CheckHurt, CheckAttack, CheckShell;
 
     private void Start()
     {
@@ -25,27 +25,34 @@ public class CrustaceanBehavior : EnemyBehaviorTree
         CheckSpawnRange = new Leaf("Player in Spawn Range?", checkSpawnRange);
         CheckAggroRange = new Leaf("Player in Aggro Range?", checkAggroRange);
         CheckAngleRange = new Leaf("Player in Attack Range?", checkAngleRange);
+        CheckBehind = new Leaf("Player behind Enemy?", checkBehind);
         MoveTowards = new Leaf("Move towards player", moveTowards);
         MoveReset = new Leaf("Reset Move", moveReset);
         AttackBasic = new Leaf("Attack", attackBasic);
+        AttackSecondary = new Leaf("Hide in Shell", attackSecondary);
 
         //Setup sequence nodes and root
         CheckPlayer = new Sequence("Player Location Sequence", CheckSpawnRange, CheckAggroRange, MoveTowards);
         CheckHurt = new Sequence("Check Hurt Sequence", CheckEnemyHurt, MoveTowards);
         CheckAttack = new Sequence("Attack Sequence", CheckAngleRange, AttackBasic);
+        CheckShell = new Sequence("Hiding Seqence", CheckBehind, AttackSecondary);
         crustaceanBehaviorTree = new BehaviorTree(MoveReset, CheckPlayer, CheckHurt, CheckAttack);
     }
 
     private void Update()
     {
         crustaceanBehaviorTree.behavior();
+        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Shell"))
+            invincible = true;
+        else
+            invincible = false;
     }
 
     public override Node.STATUS attackBasic()
     {
         if (!isAttackCD && AttackBasic.status != Node.STATUS.RUNNING)
         {
-            animator.SetInteger("AttackNum", Random.Range(0, 2));
+            animator.SetInteger("AttackNum", Random.Range(0, 3));
             animator.SetTrigger("Attack");
             AttackBasic.status = Node.STATUS.RUNNING;
         }
@@ -56,5 +63,26 @@ public class CrustaceanBehavior : EnemyBehaviorTree
             AttackBasic.status = Node.STATUS.SUCCESS;
         }
         return AttackBasic.status;
+    }
+
+    public override Node.STATUS attackSecondary()
+    {
+        if (!isAttackCD && AttackBasic.status != Node.STATUS.RUNNING)
+        {
+            animator.SetInteger("AttackNum", 2);
+            animator.SetTrigger("Attack");
+            AttackBasic.status = Node.STATUS.RUNNING;
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).loop)
+        {
+            if (!isAttackCD)
+                StartCoroutine("atttackCDEnd");
+            AttackBasic.status = Node.STATUS.SUCCESS;
+        }
+        return AttackBasic.status;
+    }
+
+    public override void counter()
+    {
     }
 }
