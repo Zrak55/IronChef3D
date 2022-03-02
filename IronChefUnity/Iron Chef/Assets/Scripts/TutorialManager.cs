cf89c446a51ac91b606a9209edfbc07558b52f45
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2022-03-02 08:41:55 -0600

    kitchen adjustments, tutorial additions

--- IronChefUnity/Iron Chef/Assets/Scripts/TutorialManager.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/TutorialManager.cs
@@ -13,8 +13,12 @@ public class TutorialManager : MonoBehaviour
     public GameObject continueBtn;
     bool on = false;
 
+    bool waiting = true;
+    bool first = true;
+
 
     public Image image;
+    public Image speechCircle;
 
     IEnumerator fader = null;
 
@@ -22,12 +26,35 @@ public class TutorialManager : MonoBehaviour
     void Start()
     {
         IronChefUtils.TurnOffCharacter();
+        image.color = Color.clear;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(first)
+        {
+            if(waiting)
+            {
+                if (FindObjectOfType<AppliancePowerSelection>() == null)
+                    return;
+                else
+                    waiting = false;
+            }
+            if(FindObjectOfType<AppliancePowerSelection>() != null)
+            {
+                image.color = Color.clear;
+                speechCircle.color = image.color;
+            }
+            else
+            {
+                Color newC = Color.white;
+                newC.a = 0;
+                image.color = newC;
+                SetImage(image.sprite);
+                first = false;
+            }
+        }
     }
 
     public void SetImage(Sprite i)
@@ -52,6 +79,7 @@ public class TutorialManager : MonoBehaviour
             while (image.color.a > 0)
             {
                 image.color = new Color(image.color.r, image.color.g, image.color.b, Mathf.Clamp(image.color.a - (2*Time.deltaTime), 0, 1));
+                speechCircle.color = image.color;
                 yield return null;
             }
             image.sprite = null;
@@ -62,6 +90,7 @@ public class TutorialManager : MonoBehaviour
         while (image.color.a < 1)
         {
             image.color = new Color(image.color.r, image.color.g, image.color.b, Mathf.Clamp(image.color.a + (2*Time.deltaTime), 0, 1));
+            speechCircle.color = image.color;
             yield return null;
         }
 
@@ -72,6 +101,7 @@ public class TutorialManager : MonoBehaviour
         while(image.color.a > 0)
         {
             image.color = new Color(image.color.r, image.color.g, image.color.b, Mathf.Clamp(image.color.a - (2*Time.deltaTime), 0, 1));
+            speechCircle.color = image.color;
             yield return null;
         }
         image.sprite = null;
