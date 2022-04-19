39f0378f927c94cbc7c517edcd527340ffdb130c
Zrak55 <45881874+Zrak55@users.noreply.github.com>
2022-04-19 14:59:33 -0500

    Aiming tweak

--- IronChefUnity/Iron Chef/Assets/Scripts/Player/CharacterMover.cs
+++ IronChefUnity/Iron Chef/Assets/Scripts/Player/CharacterMover.cs
@@ -161,15 +161,6 @@ public class CharacterMover : MonoBehaviour
 
 
         //Rotation of model
-        if (direction != Vector3.zero)
-        {
-            var oldRot = model.transform.rotation;
-            model.transform.LookAt(transform.position + direction);
-            targetRotation = model.transform.rotation;
-            model.transform.rotation = oldRot;
-        }
-        model.transform.rotation = Quaternion.RotateTowards(model.transform.rotation, targetRotation, modelRotSpeed * Time.deltaTime);
-
         if(camControl.aiming)
         {
             float rotateMult = 6;
@@ -181,7 +172,19 @@ public class CharacterMover : MonoBehaviour
             float amountToRotate = InputControls.controls.Gameplay.RotateCamera.ReadValue<Vector2>().x * rotateMult;
 
             model.transform.rotation = Quaternion.RotateTowards(model.transform.rotation, Quaternion.Euler(model.transform.rotation.eulerAngles + new Vector3(0, amountToRotate, 0)), modelRotSpeed * Time.deltaTime);
+            
             targetRotation = model.transform.rotation;
+        }
+        else
+        {
+            if (direction != Vector3.zero)
+            {
+                var oldRot = model.transform.rotation;
+                model.transform.LookAt(transform.position + direction);
+                targetRotation = model.transform.rotation;
+                model.transform.rotation = oldRot;
+            }
+            model.transform.rotation = Quaternion.RotateTowards(model.transform.rotation, targetRotation, modelRotSpeed * Time.deltaTime);
         }
 
         if(MouseOff && IronChefUtils.MouseIsHidden() == false)
